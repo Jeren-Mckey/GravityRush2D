754dad13530a8c5223934ac1c464a830b9e593ee
Jeren <jeren.mckey@sjsu.edu>
2019-08-24 19:50:35 -0700

    Player Movement Change

--- GravityRush2D/Assets/Scripts/PlayerMove.cs
+++ GravityRush2D/Assets/Scripts/PlayerMove.cs
@@ -8,16 +8,12 @@ public class PlayerMove : MonoBehaviour
     public float playerMovement;
     public float playerJumpSpeed;
     private bool gravFlipped;
-    private Vector3 velocity;
-    private Vector2 input;
     private float distToGround;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        playerJumpSpeed = 5;
-        playerMovement = 4;
         gravFlipped = false;
         distToGround = gameObject.GetComponent<Collider2D>().bounds.extents.y;
     }
@@ -25,19 +21,42 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        getInput();
-        playerRun();
-        playerJump();
-        if (Input.GetButtonDown("F"))
-        { 
+        
+    }
+
+    void FixedUpdate()
+    {
+        //Gravity Switch
+        if (Input.GetKeyDown(KeyCode.V))
+        {
             gameObject.GetComponent<Rigidbody2D>().gravityScale = -gameObject.GetComponent<Rigidbody2D>().gravityScale;
             gravFlipped = !gravFlipped;
         }
-    move(velocity, Time.deltaTime);
+
+        //Player Movement
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * playerMovement * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * playerMovement * Time.deltaTime);
+        }
+
+        //Player Jumps
+        if (Input.GetKeyDown(KeyCode.UpArrow) && !gravFlipped && isGrounded())
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 5), ForceMode2D.Impulse);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow) && gravFlipped && isGrounded())
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -5), ForceMode2D.Impulse);
+        }
     }
 
-
-    bool isGrounded()
+    private bool isGrounded()
     {
         RaycastHit2D hit =  Physics2D.Raycast(transform.position, -Vector2.up, distToGround + .1f);
         if (hit.collider != null)
@@ -47,46 +66,5 @@ public class PlayerMove : MonoBehaviour
         else return false;
     }
 
-    void getInput()
-    {
-        //Get the axis input for our character.
-        float horizontalAxis = Input.GetAxisRaw("Horizontal") * playerMovement;
-        float verticalAxis = Input.GetAxisRaw("Vertical") * playerMovement;
-
-        // Just store the input to be used elsewhere.
-        input = new Vector2(horizontalAxis, verticalAxis);
-    }
-
-    void playerJump()
-    {
-        if (isGrounded())
-        {
-            if (Input.GetKeyDown(KeyCode.UpArrow) && !gravFlipped)
-            {
-                velocity.y = playerJumpSpeed;
-            }
-            else if (Input.GetKeyDown(KeyCode.UpArrow) && gravFlipped)
-            {
-                velocity.y = -playerJumpSpeed;
-            }
-            else velocity.y = 0;
-        }
-        
-    }
-
-    void playerRun()
-    {
-        //If our character is grounded then we can execute the following code.
-        if (isGrounded())
-        {
-            // Now, run at that speed (with just a little bit of smoothing).
-            float targetVx = playerMovement * input.x;
-            velocity.x = Mathf.MoveTowards(velocity.x, targetVx, 10 * Time.deltaTime);
-        }
-    }
-
-    void move(Vector3 velocity, float t)
-    {
-        transform.Translate(velocity * t);
-    }
+    
 }
