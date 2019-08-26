140343d5e11be9107eacfc29706f1c6b6730677f
Jeren <jeren.mckey@sjsu.edu>
2019-08-25 23:44:08 -0700

    Added enemy patrol movement

---
+++ GravityRush2D/Assets/Scripts/EnemyPatrollController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyPatrollController : MonoBehaviour
+{
+    private Vector3 returnTarget;
+    public Transform target; 
+    private bool reachedEndOfPath;
+    private float distance;
+    private Vector3 direction;
+    public float speed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        distance = VectorMath.SqrDistanceXZ(transform.position, target.position);
+        direction = (target.position - transform.position);
+        returnTarget = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        distance = VectorMath.SqrDistanceXZ(target.position, transform.position);
+        Debug.Log(distance);
+        if (distance >= 0 && distance < .01)
+        {
+            Vector3 temp = target.position;
+            target.position = returnTarget;
+            returnTarget = temp;
+            direction = (target.position - transform.position);
+        }
+        else
+        {
+            transform.Translate(direction * Time.deltaTime * speed);
+        }
+    }
+}
---
+++ GravityRush2D/Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{ 
+    public delegate void OnPlayerHitDelegate();
+    public static event OnPlayerHitDelegate playerHitDelegate;
+    public static int CurrentLevel = 1;
+
+    public static void OnPlayerHit()
+    {
+        playerHitDelegate();
+    }
+    
+}
